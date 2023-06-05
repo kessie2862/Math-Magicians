91a2f8f43b3c47c6bd3427446407918df6bf538e
kessie <kayp3477@gmail.com>
2023-06-05 14:09:38 +0000

    Setup project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My First Microverse React Application
+          My First Microverse React App
         </p>
         <a
           className="App-link"

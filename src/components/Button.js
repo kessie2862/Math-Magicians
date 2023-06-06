d58d6ef9d9886907ecd9637c2da8e234f8d0ebb0
kessie <kayp3477@gmail.com>
2023-06-06 18:23:04 +0000

    Add button component

---
+++ src/components/Button.js
@@ -0,0 +1,69 @@
+import './Calculator.css';
+
+function Button() {
+  return (
+    <div className="container">
+      <div className="btns">
+        <button className="btn" type="button" value="AC">
+          AC
+        </button>
+        <button className="btn" type="button" value="+/-">
+          +/-
+        </button>
+        <button className="btn" type="button" value="%">
+          %
+        </button>
+        <button className="btn operator" type="button" value="÷">
+          ÷
+        </button>
+        <button className="btn" type="button" value="7">
+          7
+        </button>
+        <button className="btn" type="button" value="8">
+          8
+        </button>
+        <button className="btn" type="button" value="9">
+          9
+        </button>
+        <button className="btn operator" type="button" value="x">
+          x
+        </button>
+        <button className="btn" type="button" value="4">
+          4
+        </button>
+        <button className="btn" type="button" value="5">
+          5
+        </button>
+        <button className="btn" type="button" value="6">
+          6
+        </button>
+        <button className="btn operator" type="button" value="-">
+          -
+        </button>
+        <button className="btn" type="button" value="1">
+          1
+        </button>
+        <button className="btn" type="button" value="2">
+          2
+        </button>
+        <button className="btn" type="button" value="3">
+          3
+        </button>
+        <button className="btn operator" type="button" value="+">
+          +
+        </button>
+        <button className="btn span-2" type="button" value="0">
+          0
+        </button>
+        <button className="btn" type="button" value=".">
+          .
+        </button>
+        <button className="btn operator" type="button" value="=">
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Button;

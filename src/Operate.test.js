693f4aca9561d59685b1a17aabf9cd7d4a8d32a9
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-14 17:56:13 +0300

    Add division and modulo test

--- src/Operate.test.js
+++ src/Operate.test.js
@@ -20,4 +20,23 @@ describe('operate', () => {
     const result = operate(6, 2, '÷');
     expect(result).toBe('3');
   });
+
+  it('should handle division by 0', () => {
+    const result = operate(6, 0, '÷');
+    expect(result).toBe("Can't divide by 0.");
+  });
+
+  it('should return the correct result for modulo', () => {
+    const result = operate(7, 3, '%');
+    expect(result).toBe('1');
+  });
+
+  it('should handle modulo with division by 0', () => {
+    const result = operate(7, 0, '%');
+    expect(result).toBe("Can't find modulo as can't divide by 0.");
+  });
+  
+  it('should throw an error for unknown operations', () => {
+    expect(() => operate(2, 3, '^')).toThrow("Unknown operation '^'");
+  });
 });
